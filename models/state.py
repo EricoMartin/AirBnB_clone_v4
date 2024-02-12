57b870f84193e48d4095f8e9a5787aa886623969
EricoMax <47243793+EricoMartin@users.noreply.github.com>
2024-02-12 19:29:02 +0100

    Add files via upload

---
+++ models/state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+import os
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade='delete')
+    else:
+        name = ""
+
+    if storage_type != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
